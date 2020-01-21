9f8b963126d0d1b7379491af36c8c241bb811071
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-01-21 13:14:17 -0500

    begin working on UI

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import tw from 'tailwind.macro';
+import styled from 'styled-components';
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>Link1</li>
+      <li>Link2</li>
+      <li>Link3</li>
+    </ul>
+  );
+};
