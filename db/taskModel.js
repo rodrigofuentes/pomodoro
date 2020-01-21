2a12d6d0f44d9d04f0ee5d4a607b5e973db9a9f2
Keith Cagney <keithrcagney@gmail.com>
2020-01-21 14:35:04 -0500

    Building out initial models for database
    Co-authored-by: Chris Romano chrispaulromano@gmail.com

---
+++ db/taskModel.js
@@ -0,0 +1,3 @@
+const { Pool } = require('pg');
+
+const URI='postgres://vqyznmmk:8z8vS-EHRZ991BHXcSuWUbpV8CPrwpga@rajje.db.elephantsql.com:5432/vqyznmmk';
--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // TODO: implement express static
-// this is body parser
-app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../src/index.html')));
 
-// send on root
-app.get('/', (req, res) => res.send('Hello World'));
+// // send on root
+// app.get('/', (req, res) => {
+//   res.sendFile(index.html);
+// });
 
 // listen on 3000
 app.listen(3000, () => console.log('on 3000'));
