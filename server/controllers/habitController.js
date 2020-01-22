0aeedfa2f99295e7755f12f130634eaa0a917bb6
Chris Romano <chrispaulromano@gmail.com>
2020-01-22 10:36:30 -0500

    habit middleware/routing dialed in
    Co-authored-by: Keith Cagney keithrcagney@gmail.com

--- server/controllers/habitController.js
+++ server/controllers/habitController.js
@@ -1,11 +1,56 @@
+const db = require('../../db/connect');
+
 const habitController = {};
 
-habitController.getHabits = (req, res, next) => {};
+habitController.getHabits = (req, res, next) => {
+  const queryString = 'SELECT * FROM habits';
+  db.query(queryString, (err, data) => {
+    if (err) {
+      return next({
+        log: 'Error getting Habits from DB.  See noteController.getHabits',
+        message: 'Error getting Habits from DB.  See noteController.getHabits',
+      });
+    }
+    console.log(`Successfully retrieved ${data.rows.length} habits from DB`);
+    res.locals.habits = data.rows;
+    return next();
+  });
+};
 
-habitController.addHabit = (req, res, next) => {};
+habitController.addHabit = (req, res, next) => {
+  const { habit } = req.body;
+  const values = [habit];
+  const queryString = 'INSERT INTO habits (habit) VALUES ($1) RETURNING *';
+  db.query(queryString, values, (err, data) => {
+    if (err) {
+      return next({
+        log: 'Error getting Habits from DB.  See habitController.getHabits',
+        message: 'Error getting Habits from DB.  See habitController.getHabits',
+      });
+    }
+    console.log('Successfully added habit to DB: ', data.rows);
+    res.locals.habit = data.rows;
+    return next();
+  });
+};
 
 habitController.updateHabit = (req, res, next) => {};
 
-habitController.deleteHabit = (req, res, next) => {};
+habitController.deleteHabit = (req, res, next) => {
+  const { id } = req.body;
+  const values = [id];
+  const queryString = 'DELETE FROM habits WHERE id = $1 RETURNING *';
+  db.query(queryString, values, (err, data) => {
+    if (err) {
+      return next({
+        log: 'Error deleting Habits from DB.  See habitController.getHabits',
+        message: 'Error deleting Habits from DB.  See habitController.getHabits',
+      });
+    }
+    console.log('Successfully added habit to DB: ', data.rows);
+    res.locals.habit = data.rows;
+    return next();
+  });
+};
 
 module.exports = habitController;
--- server/routers/habitRouter.js
+++ server/routers/habitRouter.js
@@ -1,28 +1,30 @@
 const express = require('express');
+
 const habitRouter = express.Router();
+const habitController = require('../controllers/habitController');
 
-//handle Habit retrieval
-habitRouter.get('/habit', (req, res) => {
-  console.log(`habitRouter: Inside GET`);
+// handle Habit retrieval
+habitRouter.get('/', habitController.getHabits, (req, res) => {
+  console.log('habitRouter: Inside GET');
+  res.status(200).json(res.locals.habits);
+});
+
+// handle adding a Habit
+habitRouter.post('/', habitController.addHabit, (req, res) => {
+  console.log('habitRouter: Inside POST');
+  res.status(200).json(res.locals.habit);
+});
+
+// handle updating a Habit....but do we even have/need this functionality?
+habitRouter.put('/', habitController.updateHabit, (req, res) => {
+  console.log('habitRouter: Inside UPDATE');
   res.status(200).json();
 });
 
-//handle adding a Habit
-habitRouter.post('/habit', (req, res) => {
-  console.log(`habitRouter: Inside POST`);
-  res.status(200).json();
-});
-
-//handle updating a Habit
-habitRouter.put('/habit', (req, res) => {
-  console.log(`habitRouter: Inside UPDATE`);
-  res.status(200).json();
-});
-
-//handle deleting a Habit
-habitRouter.delete('/habit', (req, res) => {
-  console.log(`habitRouter: Inside DELETE`);
-  res.status(200).json();
+// handle deleting a Habit
+habitRouter.delete('/', habitController.deleteHabit, (req, res) => {
+  console.log('habitRouter: Inside DELETE');
+  res.status(200).json(res.locals.habit);
 });
 
 module.exports = habitRouter;
